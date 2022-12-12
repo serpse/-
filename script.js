7e62e65f8184099126d6aeb4989a8d1123f81337
serpsergej <serpsergej08@gmail.com>
2022-12-12 18:41:47 +0200

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-prompt
-alert
+const a = +prompt('ввод цифры');
+const c = +prompt('ввод цифры');
+alert (`//Користувач ввів 2 і 2;
+  ${a} + ${c} = ${a + c}
+  ${a} - ${c} = ${a - c}
+  ${a} * ${c} = ${a * c}
+  ${a} / ${c} = ${a / c}`
+  );
